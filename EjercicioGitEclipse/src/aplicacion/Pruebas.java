2ae30e53297c2872fae42a5a7857163eaf548f5c
DavidVillagrasa <david2003villa@gmail.com>
2023-01-14 11:04:51 +0100

    Primer commit forzado

--- EjercicioGitEclipse/src/aplicacion/Pruebas.java
+++ EjercicioGitEclipse/src/aplicacion/Pruebas.java
@@ -4,7 +4,7 @@ public class Pruebas {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola mundo algo");
+		System.out.println("Hola mundo algo más");
 	}
 
 }
